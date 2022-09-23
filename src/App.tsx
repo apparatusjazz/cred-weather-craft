2691044870487dc7aa248641e77c53817413812d
Maui Arcuri <maui.arcuri@care.com>
2022-09-23 17:03:02 -0500

    added tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-amber-50 h-auto">
+      <h1 className="text-3xl underline mb-2 accent-blue-800">
+        Hello world!
+      </h1>
     </div>
   );
 }
